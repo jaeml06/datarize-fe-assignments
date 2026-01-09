cb7c1bd21444cf8e75d3d0a688bbf490db176ade
jaeml06 <jaeml0630@gmail.com>
2026-01-09 12:00:21 +0900

    feat: NavBar를 포함한 DefaultLayout 구현

---
+++ apps/frontend/src/layouts/DefaultLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import NavBar from '@/components/layout/NavBar'
+
+export default function DefaultLayout() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <NavBar />
+      <main className="flex-1 max-w-7xl w-full mx-auto py-6 sm:px-6 lg:px-8">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
